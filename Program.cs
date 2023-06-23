e1e4edb8b010a4441770d70d712db64c3ac68d86
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-24 00:43:44 +0530

    [Divya]: Print Mood Happy or Sad

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace MoodAnalyser
+{
+    public class Program
+    {
+        public static string Happy_Mood = "I am In happy Mood";
+        public static string Sad_Mood = "I am In Sad Mood";
+
+        public static void Main(string[] args)
+        {
+            Mood obj = new Mood();
+            obj.AnalyseMood(Happy_Mood);
+            obj.AnalyseMood(Sad_Mood);
+        }
+    }
+}
